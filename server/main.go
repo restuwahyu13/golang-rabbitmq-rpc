16c6b323de5868be1b16daf1c462fe5ac8eb4122
restuwahyu13 <restuwahyu13@gmail.com>
2023-02-12 16:50:46 +0700

    Publish files

--- server/main.go
+++ server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -9,32 +8,16 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/jaswdr/faker"
-	"github.com/lithammer/shortuuid"
-
 	"github.com/restuwahyu13/go-rabbitmq-rpc/pkg"
 )
 
 func main() {
 	var (
 		queue string = "account"
-		data         = map[string]interface{}{}
-		fk           = faker.New()
 	)
 
-	data["id"] = shortuuid.New()
-	data["name"] = fk.App().Name()
-	data["country"] = fk.Address().Country()
-	data["city"] = fk.Address().City()
-	data["postcode"] = fk.Address().PostCode()
-
-	bodyByte, err := json.Marshal(data)
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
-
 	rabbit := pkg.NewRabbitMQ()
-	rabbit.ConsumerRpc(queue, bodyByte)
+	rabbit.ConsumerRpc(queue, nil)
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, syscall.SIGHUP, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGALRM)
